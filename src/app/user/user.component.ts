8fad8c14092212ddeb8f53b7a1cbf4da3aa253a4
NirmalaBhingare-123 <bhingare7nirmala@gmail.com>
2021-01-23 23:51:01 +0530

    Adding user details in user page

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  selector: "app-user",
+  templateUrl: "./user.component.html",
+  styleUrls: ["./user.component.css"]
 })
 export class UserComponent implements OnInit {
+  c1: boolean = false;
+  c2: boolean = false;
+  c3: boolean = false;
+  c4: boolean = false;
 
-  constructor() { }
+  city = [
+    { id: 1, name: "Pune" },
+    { id: 2, name: "Mumbai" },
+    { id: 3, name: "Delhi" },
+    { id: 4, name: "surat" }
+  ];
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
